95e4e595ebeab8e19d31fc0e148f5b583499d72e
gpichelli <33701562+gpichelli@users.noreply.github.com>
2024-12-21 09:43:42 -0500

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -9,13 +9,13 @@ st.write(
     """Choose the fruits you want in your custom smoothie! 
     """
 )
-cnx = st.connection("Snowflake")
+cnx = st.connection("snowflake")
 session = cnx.session()
 
 name_on_order = st.text_input("Name on Smoothie: ")
 st.write("The name on your Smoothie will be: ", name_on_order)
 
-session = get_active_session()
+# session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 
