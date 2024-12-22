d5b4106c209e838b70941c851de1c60e6a1ec716
gpichelli <33701562+gpichelli@users.noreply.github.com>
2024-12-22 11:33:48 -0500

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -21,10 +21,6 @@ st.write("The name on your Smoothie will be: ", name_on_order)
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 
-pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
-
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients: '
      , my_dataframe 
@@ -32,10 +28,14 @@ ingredients_list = st.multiselect(
 
 if ingredients_list:
     ingredients_string = ''
-
+    
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-        
+            
+        pd_df = my_dataframe.to_pandas()
+        st.dataframe(pd_df)
+        st.stop()
+       
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
 
