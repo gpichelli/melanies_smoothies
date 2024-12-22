cd935a811aa0844d57f01a2f0eb2f5a6c1d6c7e8
gpichelli <33701562+gpichelli@users.noreply.github.com>
2024-12-22 12:07:27 -0500

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -42,8 +42,8 @@ if ingredients_list:
         st.subheader(fruit_chosen + ' Nutritional Information')
         # smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-        st.text(smoothiefroot_response.json) 
-        # sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+        # st.text(smoothiefroot_response.json) 
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
         # st.stop()
 
     # st.write(ingredients_string)
