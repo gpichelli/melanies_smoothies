dbd10d5c8a63c3122512285ff31181b88fa987b2
gpichelli <33701562+gpichelli@users.noreply.github.com>
2024-12-22 12:32:21 -0500

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -39,8 +39,9 @@ if ingredients_list:
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         # st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         if search_on:
-            search = search_on
-        else search = fruit_chosen:
+                search = search_on
+        else:
+                search = fruit_chosen
 
         st.subheader(fruit_chosen + ' Nutritional Information')
         # smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
