a3c9b92efe3c7b9e9f95df9858a0527092af8053
gpichelli <33701562+gpichelli@users.noreply.github.com>
2024-12-22 12:28:10 -0500

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -40,7 +40,7 @@ if ingredients_list:
         # st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         if search_on:
             search = search_on
-        else fruit_chosen
+        else search = fruit_chosen:
 
         st.subheader(fruit_chosen + ' Nutritional Information')
         # smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
