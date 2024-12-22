a220ef8716c82d820e48756fc191b4a996a6c105
gpichelli <33701562+gpichelli@users.noreply.github.com>
2024-12-22 10:54:27 -0500

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -17,8 +17,10 @@ name_on_order = st.text_input("Name on Smoothie: ")
 st.write("The name on your Smoothie will be: ", name_on_order)
 
 # session = get_active_session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
+
+pd_df = my_dataframe.to_panda()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients: '
@@ -34,7 +36,8 @@ if ingredients_list:
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
         # st.text(smoothiefroot_response.json) 
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
-    
+        st.stop()
+
     # st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
